fab1b7936ac43329348158172b028caf2f249111
Samuel Amado Ramirez <samuelamadoramirez@gmail.com>
2021-07-08 08:00:40 -0500

    configuring the right path in server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/mq135-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/mq135-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
