397d4b1c6030329cf9c4a0e14e85a9f3f0cbe231
Samuel Amado Ramirez <samuelamadoramirez@gmail.com>
2021-07-08 09:04:21 -0500

    correct home path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/mq135-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mq135-frontend/'}),
+    res.sendFile('home.component.html', {root: 'dist/mq135-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
